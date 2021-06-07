673d76585f72cfa51c0c7b6204869346ce0c641f
false-fundev <50541530+false-fundev@users.noreply.github.com>
2021-06-08 00:26:02 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,17 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install git -y
 
-git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
+git clone https://github.com/Chia-Network/chia-blockchain.git -b pools.2021-may-25 --recurse-submodules
 cd chia-blockchain
 
 sh install.sh
+
+cd ../
+
+git clone https://github.com/Chia-Network/pool-reference.git
+
+cd pool-reference
+python3.8 -m venv ./venv
+source ./venv/bin/activate
+pip install ../chia-blockchain/ 
+sudo CHIA_ROOT="/your/home/dir/.chia/testnet7" ./venv/bin/python pool/pool_server.py
