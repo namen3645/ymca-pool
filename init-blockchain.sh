aaf1cbfbb562b45ce53f99c532311c2d33bd9e24
namen3645 <62731881+namen3645@users.noreply.github.com>
2021-06-08 11:26:26 +0300

    Update and rename init.sh to init-blockchain.sh

---
+++ init-blockchain.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt install python3.8 pip -y
+
+sudo apt install git -y
+
+git clone https://github.com/Chia-Network/chia-blockchain.git -b pools.2021-may-25 --recurse-submodules
+cd chia-blockchain
+
+sh install.sh
