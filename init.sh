fc0cef62fbc983449feb2afd02db9e98df4f7fe3
namen3645 <62731881+namen3645@users.noreply.github.com>
2021-06-08 10:50:46 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install git -y
 
+sudo apt install python3.8 pip -y
+
 git clone https://github.com/Chia-Network/chia-blockchain.git -b pools.2021-may-25 --recurse-submodules
 cd chia-blockchain
 
